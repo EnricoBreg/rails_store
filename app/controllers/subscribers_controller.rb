7549fbccd9ff8e9c85f42717cbed44abb2696827
ebregoli <bregolienrio@gmail.com>
2026-03-17 17:52:37 +0100

    Aggiunta possibilità di fare subscribe quando il prodotto ritorna disponibile

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,22 @@
+class SubscribersController < ApplicationController
+  allow_unauthenticated_access
+  before_action :set_product
+
+  # POST /products/:id/subscribers
+  def create
+    if @product.subscribers.where(subscriber_params).first_or_create
+      redirect_to product_path(@product), notice: "You are now subscribed to this product."
+    else
+      redirect_to product_path(@product), alert: "There was a problem subscribing to this product. Please try again."
+    end
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def subscriber_params
+      params.expect(subscriber: [ :email ])
+    end
+end
