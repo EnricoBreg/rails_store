9104d98e2cd53cfa8df8fa7cab841d0e3696d564
ebregoli <bregolienrico@gmail.com>
2026-03-18 12:28:51 +0100

    Aggiunto rate_limit al controller SignUpsController

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,6 +1,8 @@
 class SignUpsController < ApplicationController
   unauthenticated_access_only
 
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to sign_up_path, alert: "This is not possible now. Try again later." }
+
   def show
     @user = User.new
   end
