b66fadf193da474a3b8cfe7eeeef5f1f6c59fc4e
ebregoli <bregolienrico@gmail.com>
2026-03-19 12:09:25 +0100

    Corretto aggiornamento dell'email nel controller e nel form

--- app/controllers/settings/emails_controller.rb
+++ app/controllers/settings/emails_controller.rb
@@ -6,6 +6,8 @@ class Settings::EmailsController < Settings::BaseController
     user = Current.user
     if user.update(email_params)
       redirect_to settings_email_path, status: :see_other, notice: "Email updated successfully. We've sent a verification email to #{user.unconfirmed_email}"
+    else
+      render :show, status: :unprocessable_entity
     end
   end
 
