ff904f92edd97f79527d9ad892242cc4a1b8833b
ebregoli <bregolienrico@gmail.com>
2026-03-21 15:22:12 +0100

    Aggiunti test per cambiare la nuova email, confermare la nuova email inserita e testare l'invio corretto della mail di conferma

--- test/controllers/email/confirmations_controller_test.rb
+++ test/controllers/email/confirmations_controller_test.rb
@@ -1,8 +1,37 @@
 require "test_helper"
 
 class Email::ConfirmationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get email_confirmations_show_url
-    assert_response :success
+  # Test no. 1
+  # Questo test verifica che se viene fornito un token non valido, l'email dell'utente
+  # non deve di conseguenza essere aggiornata, e deve essere restituito un messaggio
+  # di errore che indica che il link di conferma è invalido o è scaduto.
+  # Inoltre, viene anche verificato che l'email dell'utente non venga effettivamente
+  # aggiornata, e rimanga uguale a quella precedente.
+  test "invalid token are ignored" do
+    user = users(:one)
+    previous_email = user.email_address
+    user.update(unconfirmed_email: "new@example.com")
+    get email_confirmation_path(token: "invalid")
+    # assert_equal "Invalid token.", flash[:alert]
+    assert_equal "The email confirmation link is invalid or has expired.", flash[:alert]
+    user.reload
+    assert_equal previous_email, user.email_address
+  end
+
+  # Test no. 2
+  # Questo test verifica se viene fornita un token valido. L'email dell'utente deve
+  # essere aggiornata con successo usando la nuova email non confermata, e deve essere
+  # restituito un messaggio di successo che indica che l'indirizzo email è stato verificato
+  # con successo. Inoltre, viene anche verificato che l'attributo :unconfirmed_email dell'utente
+  # venga azzerato (nil) dopo la conferma, e che l'email dell'utente sia effettivamente aggiornata
+  # con la nuova email inserita in :unconfirmed_email.
+  test "email is updated with a valid token" do
+    user = users(:one)
+    user.update(unconfirmed_email: "new@example.com")
+    get email_confirmation_path(token: user.generate_token_for(:email_confirmation))
+    assert_equal "Your email address has been verified successfully.", flash[:notice]
+    user.reload
+    assert_equal "new@example.com", user.email_address
+    assert_nil user.unconfirmed_email
   end
 end
--- test/controllers/settings/emails_controller_test.rb
+++ test/controllers/settings/emails_controller_test.rb
@@ -4,4 +4,33 @@ class Settings::EmailsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  # Test no. 1
+  # Questo test serve per verificare la validazione della password corrente quando
+  # un utente cerca di aggiornare la propria email. Se la password fornita è errata,
+  # l'aggiornamento dell'email non deve avvenire, e deve essere resituito un errore di
+  # validazione (422 Unprocessable Entity).
+  # Inoltre, non deve esere inviata nessuna email di conferma per la nuova email inserita.
+  test "validates current password" do
+    user = users(:one)
+    sign_in_as user
+    patch settings_email_path, params: { user: { password_challenge: "invalid_password", unconfirmed_email: "newemail@exampe.com" } }
+    assert_response :unprocessable_entity # la validazione deve fallire e restituire un errore 422
+    assert_nil user.reload.unconfirmed_email # unconfirmed_email deve essere nil
+    assert_no_emails # non deve essere inviata nessuna email di conferma per la nuova email
+  end
+
+  # Test no. 2
+  # Questo test, esegue la verifica contraria del Test no. 1. Cioè verifica che la password corrente
+  # sia valida, viene rendirizzato l'utente, la mail non ancora confermata (:unconfirmed_email) è uguale
+  # a quella inserita nella richiesta di update ed, infine, viene verificato che UserMail abbia in coda
+  # la mail di da inviare al nuovo indirizzo inserito per la conferma.
+  test "sends email confirmation on successful update" do
+    user = users(:one)
+    sign_in_as user
+    patch settings_email_path, params: { user: { password_challenge: "password", unconfirmed_email: "new@example.com" } }
+    assert_response :redirect
+    assert_equal "new@example.com", user.reload.unconfirmed_email
+    assert_enqueued_email_with UserMailer, :email_confirmation, params: { user: user }
+  end
 end
