8cb92f4c9a75aa3b839f773be20877f99e83ff0d
ebregoli <bregolienrico@gmail.com>
2026-03-22 21:22:17 +0100

    Implementata funzionalità per la quale l'admin può visualizzare/modificare i subscribers ai prodotti

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
     resources :products
     resources :users
     resources :wishlists
+    resources :subscribers
 
     root to: redirect("/store/products")
   end
