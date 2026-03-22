013d58b3c2ba7016a61248bbabe7b160f9a29e87
ebregoli <bregolienrico@gmail.com>
2026-03-22 14:25:40 +0100

    Aggiunto metodo to_param al model Wishlist per l'implementazione di URL user-friendly

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,4 +2,12 @@ class Wishlist < ApplicationRecord
   belongs_to :user
   has_many :wishlist_products, dependent: :destroy
   has_many :products, through: :wishlist_products
+
+=begin   def to_param
+    # Squish rimuove spazi extra e parameterize rende la stringa URL-friendly
+    "#{id}-#{name.squish.parameterize}"
+  end
+=end
+
+  to_param :name
 end
